ba0cc2384c4657e25367fa1a71f38eca0d38d099
karalkal <karalkal@yahoo.co.uk>
2023-03-26 02:51:17 +0100

    items mapped to components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,6 +7,7 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
+  const items = props.items
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
@@ -16,26 +17,17 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+
+        {items.map(i =>
+          <div>
+            <ExpenseItem
+              title={i.title}
+              amount={i.amount}
+              date={i.date}
+            />
+          </div>
+        )}
+
       </Card>
     </div>
   );
