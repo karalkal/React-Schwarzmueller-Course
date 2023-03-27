95facf925358d241642192bc1fbffffa8ae409bd
karalkal <karalkal@yahoo.co.uk>
2023-03-27 22:27:32 +0100

    read mock data only if nothing in localStorage at page reload

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import Expenses from './components/Expenses/Expenses';
 
 // OF COURSE you have to set localStorage here, idiot!
 // otherwise it will re-assign itself with the initial array at each App re-run
-localStorage.setItem("expenses", JSON.stringify(initialExpensesData));
+if (!localStorage.getItem("expenses")) {
+  localStorage.setItem("expenses", JSON.stringify(initialExpensesData));
+}
 
 const App = () => {
 
   const [expensesFromLocalStorage, setExpensesFromLocalStorage] =
-    React.useState(localStorage.getItem("expenses"));  
+    React.useState(localStorage.getItem("expenses"));
 
-  const addExpenseHandler = newExpense => {    
+  const addExpenseHandler = newExpense => {
 
     let prevExpArr = JSON.parse(localStorage.getItem("expenses"));
     let newExpArr = [newExpense, ...prevExpArr]
 
     // set localStorage with updated array
     localStorage.setItem("expenses", JSON.stringify(newExpArr));
-    
+
     // trigger re-render with the newly assigned state
     setExpensesFromLocalStorage(localStorage.getItem("expenses"))
   };
--- src/data.js
+++ src/data.js
@@ -1,25 +1,25 @@
 const expenses = [
   {
     id: 'e1',
-    title: 'Laptop',
+    title: 'Mock1',
     amount: 94.12,
     date: new Date(2019, 7, 14),
   },
   {
     id: 'e2',
-    title: 'TV',
+    title: 'Mock2',
     amount: 799.49,
     date: new Date(2020, 2, 1)
   },
   {
     id: 'e3',
-    title: 'Car Insurance',
+    title: 'Mock3',
     amount: 294.67,
     date: new Date(2022, 2, 28),
   },
   {
     id: 'e4',
-    title: 'Desk',
+    title: 'Mock4',
     amount: 450,
     date: new Date(2022, 5, 3),
   },
