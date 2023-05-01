d39c821ee59280301ada4f5f4c9f282782c2f65e
karalkal <karalkal@yahoo.co.uk>
2023-05-01 20:28:44 +0100

    created context

--- 06-practice--food-order-app/src/App.js
+++ 06-practice--food-order-app/src/App.js
@@ -1,12 +1,14 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useContext } from 'react';
 import Header from './components/Layout/Header';
 import MainContainer from './components/Meals/MainContainer'
 import Cart from './components/Cart/Cart';
+import cartContext from './store/cart-context';
 
 
 function App() {
   const [cartVisible, setCardVisible] = useState(false)
 
+  // showCartHandler and hideCartHandler are being passed via a chain of comps
   const showCartHandler = () => {
     setCardVisible(true)
   }
@@ -14,14 +16,31 @@ function App() {
     setCardVisible(false)
   }
 
+  // ctx functions
+  function addItemToCartHandler(item) {
+  };
+
+  function removeItemFromCartHandler(id) {
+  };
+
   return (
-    <Fragment>
+    <cartContext.Provider
+      value={{
+        items: [],
+        totalAmount: 0,
+        addItem: (addItemToCartHandler),
+        removeItem: removeItemFromCartHandler,
+      }}>
+
       {cartVisible && <Cart onHideCart={hideCartHandler} />}        {/* Cart is a Modal */}
+
       <Header onDisplayCart={showCartHandler} />
+
       <main>
         <MainContainer />
       </main>
-    </Fragment>
+
+    </cartContext.Provider>
   );
 }
 
--- 06-practice--food-order-app/src/components/Cart/Cart.js
+++ 06-practice--food-order-app/src/components/Cart/Cart.js
@@ -1,10 +1,17 @@
+import React from 'react';
+
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import cartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+
+  const ctx = React.useContext(cartContext)
+  console.log(ctx)
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+      {ctx.items.map((item) => (
         <li key={item.id}>{item.name}</li>
       ))}
     </ul>
@@ -15,7 +22,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{ctx.totalAmount.toFixed(2)}</span>
       </div>
       <div className={classes.actions}>
         <button
