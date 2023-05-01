f4c81c7784d4252ab10a5986d6be88312d396f6e
karalkal <karalkal@yahoo.co.uk>
2023-05-01 01:50:16 +0100

    created Modal generic comp, display Cart as modal

--- 06-practice--food-order-app/src/App.js
+++ 06-practice--food-order-app/src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
 import MainContainer from './components/Meals/MainContainer'
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />        {/* Cart is a Modal */}
       <Header />
       <main>
         <MainContainer />
