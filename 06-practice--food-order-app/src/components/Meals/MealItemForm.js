d40a09e701a0a12699706d3e24db237033096f71
karalkal <karalkal@yahoo.co.uk>
2023-05-01 01:05:07 +0100

    created reusable Input comp and comp Form to control quantity inputs

---
+++ 06-practice--food-order-app/src/components/Meals/MealItemForm.js
@@ -0,0 +1,30 @@
+import Input from '../UI/Input';
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+    return (
+        <form className={classes.form}>
+            <Input
+                // label takes input.id and labelName <label htmlFor={props.input.id}>{props.labelName}</label>
+                labelName='Amount'
+
+                // input takes input object and spreads its properties 
+                // <input {...props.input} /> 
+                // to create <input key1="value1" key2="value2">, 
+                // i.e. <input id="amount_undefined" type="number" min="1" max="5" step="1" value="1">
+                input={{
+                    id: 'amount',
+                    // id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1',
+                }}
+            />
+            <button>+ Add</button>
+        </form>
+    );
+};
+
+export default MealItemForm;
