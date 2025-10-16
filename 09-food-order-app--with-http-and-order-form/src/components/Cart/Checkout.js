d0163f43ad0494ff8f231695db244e85c573796a
karalkal <karalkal@yahoo.co.uk>
2025-10-16 17:32:15 +0100

    getting order form inputs

--- 09-food-order-app--with-http-and-order-form/src/components/Cart/Checkout.js
+++ 09-food-order-app--with-http-and-order-form/src/components/Cart/Checkout.js
@@ -1,27 +1,39 @@
+import { useRef } from 'react';
+
 import classes from './Checkout.module.css';
 
-function Checkout(props) {
+const Checkout = (props) => {
+    const nameInputRef = useRef();
+    const streetInputRef = useRef();
+    const postCodeInputRef = useRef();
+    const cityInputRef = useRef();
+
     const submitHandler = (event) => {
         event.preventDefault();
+
+        const nameEntry = nameInputRef.current.value;
+        const streetEntry = streetInputRef.current.value;
+        const postCodeEntry = postCodeInputRef.current.value;
+        const cityEntry = cityInputRef.current.value;
     };
 
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input type='text' id='name' ref={nameInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='street'>Street</label>
-                <input type='text' id='street' />
+                <input type='text' id='street' ref={streetInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='postal'>Post Code</label>
-                <input type='text' id='postal' />
+                <input type='text' id='postal' ref={postCodeInputRef} />
             </div>
             <div className={classes.control}>
                 <label htmlFor='city'>City</label>
-                <input type='text' id='city' />
+                <input type='text' id='city' ref={cityInputRef} />
             </div>
             <div className={classes.actions}>
                 <button type='button' onClick={props.onCancel}>
